611b667d432b69542a09ffdd67b2fb0b08654db1
jeffery 陈帆 <jeffery9@outlook.com>
2020-05-17 13:56:11 +0800

    improve test script

--- website_metaweblog/test/test_metaweblog.py
+++ website_metaweblog/test/test_metaweblog.py
@@ -2,28 +2,25 @@ import xmlrpc.client
 
 url = 'http://127.0.0.1:8069'
 
-# common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
-# version = common.version()
-# print(version)
+common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+version = common.version()
+print(version)
 
 method_lists = [
-    'blogger.deletePost',
-    'blogger.getUsersBlogs',
-    'metaWeblog.editPost',
-    'metaWeblog.getCategories',
-    'metaWeblog.getPost',
-    'metaWeblog.getRecentPosts',
-    'metaWeblog.newMediaObject',
-    'metaWeblog.newPost',
-    'wp.newCategory']
+    'blogger.getUsersBlogs','blogger.deletePost',  'metaWeblog.editPost', 'metaWeblog.getCategories',
+    'metaWeblog.getPost', 'metaWeblog.getRecentPosts', 'metaWeblog.newMediaObject', 'metaWeblog.newPost',
+    'wp.newCategory'
+]
 
 metaweblog = xmlrpc.client.ServerProxy('{}/xmlrpc/2/metaweblog'.format(url))
 
 print(metaweblog)
 
 for method in method_lists:
-    print(method)
+    print('call method: %s'%method)
     fn = eval('%s.%s' % ('metaweblog', method))
-    print(fn)
-    result = fn()
-    print(result)
+
+    params = ['var1', 'admin', 'admin']
+    result = fn(*params)
+    print('     result: {}'.format(result))
+    print('--------')
