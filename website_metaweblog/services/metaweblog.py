c1b41111f8936987e2da75d0fc81f51f15809ead
jeffery 陈帆 <jeffery9@outlook.com>
2020-05-17 13:55:06 +0800

    implement blogger.getUserBlogs

--- website_metaweblog/services/metaweblog.py
+++ website_metaweblog/services/metaweblog.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+
+import odoo
+
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -17,6 +20,28 @@ need to implement those methods
 
 '''
 
+db = odoo.http.request.db
+registry = odoo.registry(db)
+
+env = odoo.api.Environment(registry.cursor(), odoo.SUPERUSER_ID, {})
+
+if db:
+    user_model = env['res.users']
+    blog_model = env['blog.blog']
+    tag_category_model = env['blog.tag.category']
+    blog_tag_model = env['blog.tag']
+    blog_post_model = env['blog.post']
+
+
+def blog_installed():
+    if not db:
+        return False
+
+    if 'blog.blog' in env:
+        return True
+
+    return False
+
 
 class BlogInfo():
     def __init__(self, blogid, url, blogName):
@@ -25,43 +50,75 @@ class BlogInfo():
         self.blogName = blogName
 
 
-def exp_blogger_deletePost(appKey, postid, username, password, publish):
+def check_permission(username, password):
+    uid = user_model.authenticate(db, username, password, None)
+    if uid and user_model.browse(uid).has_group('website.group_website_designer'):
+        return True
+    return False
+
+
+def exp_blogger_deletePost(appKey, postid, username, password, publish=True):
+    if not check_permission(username, password):
+        return
     raise Exception("Not implemented...")
 
 
 def exp_blogger_getUsersBlogs(appKey, username, password):
+    if not check_permission(username, password):
+        return
+
+    blog_ids = blog_model.search([])
+
+    return blog_ids.read(['name', 'subtitle', 'content'])
+
     raise Exception("Not implemented...")
 
 
-def exp_metaWeblog_editPost(postid, username, password, post, publish):
+def exp_metaWeblog_editPost(postid, username, password, post, publish=True):
+    if not check_permission(username, password):
+        return
     raise Exception("Not implemented...")
 
 
 def exp_metaWeblog_getCategories(blogid, username, password):
+    if not check_permission(username, password):
+        return
     raise Exception("Not implemented...")
 
 
 def exp_metaWeblog_getPost(blogid, username, password):
+    if not check_permission(username, password):
+        return
     raise Exception("Not implemented...")
 
 
-def exp_metaWeblog_getRecentPosts(blogid, username, password, numberOfPosts):
+def exp_metaWeblog_getRecentPosts(blogid, username, password, numberOfPosts=20):
+    if not check_permission(username, password):
+        return
     raise Exception("Not implemented...")
 
 
 def exp_metaWeblog_newMediaObject(blogid, username, password, file):
+    if not check_permission(username, password):
+        return
     raise Exception("Not implemented...")
 
 
-def exp_metaWeblog_newPost(blogid, username, password, post, publish):
+def exp_metaWeblog_newPost(blogid, username, password, post, publish=True):
+    if not check_permission(username, password):
+        return
     raise Exception("Not implemented...")
 
 
 def exp_wp_newCategory(blog_id, username, password, category):
+    if not check_permission(username, password):
+        return
     raise Exception("Not implemented...")
 
 
 def dispatch(method, params):
+    if not blog_installed():
+        raise Exception("Website Blog is not installed.")
 
     g = globals()
     exp_method_name = 'exp_' + method.replace('.', '_')
