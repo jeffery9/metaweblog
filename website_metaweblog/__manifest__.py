3eebc59712d78dc110ffbc7a9480342dabf742a5
jeffery <jeffery9@outlook.com>
2023-04-03 10:27:04 +0800

    cleanup

--- website_metaweblog/__manifest__.py
+++ website_metaweblog/__manifest__.py
@@ -6,8 +6,8 @@
     'description': """
          
     """,
-    'author': "jeffery, genin IT",
-    'website': "http://www.geninit.cn",
+    'author': 'genin IT, jeffery <jeffery9@gmail.com>',
+    'website': 'http://www.geninit.cn',
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -22,5 +22,7 @@
     'data': [],
     # only loaded in demonstration mode
     'demo': [],
-    'post_load': 'post_load'
+    'post_load': 'post_load',
+    'price': 50,
+    'currency': 'EUR',
 }
--- website_metaweblog/controllers/main.py
+++ website_metaweblog/controllers/main.py
@@ -3,18 +3,3 @@ import json
 from odoo import http
 from odoo.addons.portal.controllers.web import Home
 
-class Website(Home):
-
-    @http.route(['/website/seo_suggest'], type='json', auth="user", website=True)
-    def seo_suggest(self, keywords=None, lang=None):
-        language = lang.split("_")
-        url = "http://suggestion.baidu.com/su"
-        try:
-            req = requests.get(url, params={
-                'action': 'opensearch', 'wd': keywords, })
-            req.raise_for_status()
-            response = req.text
-        except IOError:
-            return []
-        data = json.loads(response)
-        return json.dumps(data[1])
